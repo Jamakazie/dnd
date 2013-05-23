f3f31ee0fde6317ecc6dc5af7066358570067890
jama <jamakazie@gmail.com>
2013-05-23 16:33:06 -0400

    EoD Commit

--- Races/views.py
+++ Races/views.py
@@ -5,7 +5,7 @@ from Races.models import race, race_desc
 # Create your views here.
 races = race.objects.all()
 context = {}
-context['races'] = races
+context['races'] = sorted(races)
 def race_index(request):
 	return render_to_response('race_index.html', context, context_instance = RequestContext(request))
 
